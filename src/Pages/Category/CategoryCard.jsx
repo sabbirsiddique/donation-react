b4e46b1ec6e4afdb5972fa90a2ddb076408109f5
sabbirsiddique <sabbirsiddique6798@gmail.com>
2023-09-26 06:15:48 +0600

    Error handled for Netlify

--- src/Pages/Category/CategoryCard.jsx
+++ src/Pages/Category/CategoryCard.jsx
@@ -3,47 +3,28 @@
 import Swal from "sweetalert2";
 
 const CategoryCard = ({ category }) => {
-  const {
-    picture,
-    title,
-    item_text_color,
-    description,
-    price,
-  } = category || {};
+  const { picture, title, item_text_color, description, price } =
+    category || {};
 
   const itemTextStyle = {
     backgroundColor: item_text_color,
   };
 
-  const handleDonations = ()=>{
-    
+  const handleDonations = () => {
     const donationsArr = [];
 
     const donationItems = JSON.parse(localStorage.getItem("donations"));
 
-    if (!donationItems){
-        donationsArr.push(category);
-        localStorage.setItem("donations",JSON.stringify(donationsArr));
-        Swal.fire(
-            'Good job!',
-            'Donation Successful...!',
-            'success'
-          )
-    }else{
-
-
-
-
-        donationsArr.push(...donationItems,category);
-        localStorage.setItem('donations',JSON.stringify(donationsArr));
-        Swal.fire(
-          'Good job!',
-          'Donation Successful...!',
-          'success'
-          )
-
+    if (!donationItems) {
+      donationsArr.push(category);
+      localStorage.setItem("donations", JSON.stringify(donationsArr));
+      Swal.fire("Good job!", "Donation Successful...!", "success");
+    } else {
+      donationsArr.push(...donationItems, category);
+      localStorage.setItem("donations", JSON.stringify(donationsArr));
+      Swal.fire("Good job!", "Donation Successful...!", "success");
     }
-  }
+  };
 
   return (
     <div>
@@ -51,7 +32,11 @@ const CategoryCard = ({ category }) => {
         <img src={picture} className="w-full h-[70vh] object-fit" />
         <div className="absolute bottom-0 w-full h-1/5 bg-black opacity-50"></div>
         <div className="absolute bottom-0 w-full h-1/5 py-8 pl-9">
-          <button onClick={handleDonations} className="w-[179px] h-[56px] text-white rounded-lg" style={itemTextStyle}>
+          <button
+            onClick={handleDonations}
+            className="w-[179px] h-[56px] text-white rounded-lg"
+            style={itemTextStyle}
+          >
             Donate $ {price}
           </button>
         </div>
--- src/Pages/Donation/Donations.jsx
+++ src/Pages/Donation/Donations.jsx
@@ -2,34 +2,35 @@ import { useEffect, useState } from "react";
 import DonatedCard from "./DonatedCard";
 
 const Donations = () => {
+  const [donations, setDonations] = useState([]);
+  const [notFound, setNotFound] = useState(false);
+  const [isShow, setIsShow] = useState(false);
 
-    const [donations,setDonations] = useState([]);
-    const [notFound, setNotFound] = useState(false);
-    const [isShow, setIsShow] = useState(false);
+  useEffect(() => {
+    const donationItems = JSON.parse(localStorage.getItem("donations"));
 
+    if (donationItems) {
+      setDonations(donationItems);
+    } else {
+      setNotFound("No donate yet");
+    }
+  }, []);
 
-    useEffect(()=>{
-        const donationItems = JSON.parse(localStorage.getItem("donations"));
+  // const handleRemove = () => {
+  //     localStorage.clear();
+  //     setDonations([]);
+  //     setNotFound("No Data Found");
+  //   };
 
-        if(donationItems){
-            setDonations(donationItems);
-        }else{
-            setNotFound("No donate yet");
-        }
-    },[]);
-
-
-    // const handleRemove = () => {
-    //     localStorage.clear();
-    //     setDonations([]);
-    //     setNotFound("No Data Found");
-    //   };
-
-    return (
-    <div>{notFound?<div className="flex justify-center items-center text-center bg-red-500 h-[500px] w-[500px] mx-auto text-2xl text-white font-extrabold mt-36 rounded-full"><p>{notFound}</p></div>:
-    
+  return (
     <div>
-        {/* {donations.length > 0 && (
+      {notFound ? (
+        <div className="flex justify-center items-center text-center bg-red-500 h-[500px] w-[500px] mx-auto text-2xl text-white font-extrabold mt-36 rounded-full">
+          <p>{notFound}</p>
+        </div>
+      ) : (
+        <div>
+          {/* {donations.length > 0 && (
             <div>
                 <button
               onClick={handleRemove}
@@ -39,29 +40,33 @@ const Donations = () => {
             </button>
             </div>
           )} */}
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4 max-w-screen-2xl mx-auto mt-12 mb-10">
-            {isShow ? donations.map((category)=> (
-            <DonatedCard key={category} category={category}></DonatedCard>)):
-            donations.slice(0,4).map ((category)=>(
-            <DonatedCard key={category} category={category}></DonatedCard>
-            ))
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4 max-w-screen-2xl mx-auto mt-12 mb-10">
+            {isShow
+              ? donations.map((category) => (
+                  <DonatedCard key={category} category={category}></DonatedCard>
+                ))
+              : donations
+                  .slice(0, 4)
+                  .map((category) => (
+                    <DonatedCard
+                      key={category}
+                      category={category}
+                    ></DonatedCard>
+                  ))}
+          </div>
 
-            }
-        </div>
-
-        {
-            donations.length > 4 && <button onClick={()=>setIsShow(!isShow)} className="px-5 bg-green-500 block mx-auto">
-                {isShow ? "":"See More"}
+          {donations.length >= 4 && (
+            <button
+              onClick={() => setIsShow(!isShow)}
+              className="px-2 rounded-md text-white font-bold  bg-green-500 block mx-auto"
+            >
+              {isShow ? "" : "See More"}
             </button>
-        }
-        
-        
+          )}
+        </div>
+      )}
     </div>
-    }
-    
-    </div>
-        
-    );
+  );
 };
 
 export default Donations;
